c1b426eb657f1f21f03edef42ebc340d168b72d7
Dmitry Senchenko <114693652+DmitrySA1996@users.noreply.github.com>
2022-12-14 23:15:22 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,8 @@
 const popup = document.querySelector('.popup');
 const popupContainer = popup.querySelector('.popup__container');
-const openPopupButtons = document.querySelector('.popup_opened');
-const closePopupButton = document.querySelector('.popup__close');
+const popupForm = popupContainer.querySelector('.popup__form');
 
 const profileEditText = document.querySelector('.profile__edit-text');
-const profileAddButton = document.querySelector('.profile__add-button');
-
-const popupForm = popupContainer.querySelector('.popup__form');
 
 const title = document.querySelector('.profile__title');
 const subtitle = document.querySelector('.profile__subtitle');
@@ -51,36 +47,70 @@ initialCards.forEach(function (element) {
   elementCard.querySelector('.elements__image').alt = element.name;
   elementCard.querySelector('.elements__text').textContent = element.name;
 
-  elementsСards.append(elementCard)
+  elementsСards.append(elementCard);
 });
 
-function editpopup(Edit, name, subname) {
+function editpopup(Edit, name, subname, pop, nameText, info) {
   Edit.addEventListener('click', (event) => {
-    popup.classList.add('popup_opened');
-    nameInput.value = name;
-    infoInput.value = subname;
-  })
+    pop.classList.add('popup_opened');
+    nameText.value = name;
+    info.value = subname;
+  });
 };
-
-editpopup(profileEditText, title.textContent, subtitle.textContent);
-editpopup(profileAddButton, 'Название', 'Ссылка на картинку');
 
 function closePopup() {
   popup.classList.remove('popup_opened');
+  popupImage.classList.remove('popup_opened');
 };
 
-closePopupButton.addEventListener('click', closePopup);
+function handleFormSubmit(event) {
+  event.preventDefault();
+
+  title.textContent = nameInput.value;
+  subtitle.textContent = infoInput.value;
+  popup.classList.remove('popup_opened');
+};
+
+editpopup(profileEditText, title.textContent, subtitle.textContent, popup, nameInput, infoInput);
+
+popup.addEventListener('submit', handleFormSubmit);
+
+popupContainer.querySelector('.popup__close').addEventListener('click', closePopup);
+
+const popupImage = document.querySelector('.popup-image');
+const popupContainerImage = popupImage.querySelector('.popup__container');
+const popupFormImage = popupContainerImage.querySelector('.popup__form');
+const profileAddButton = document.querySelector('.profile__add-button');
+const nameImage = popupFormImage.querySelector('.popup__text_type_title');
+const linkImage = popupFormImage.querySelector('.popup__text_type_subtitle');
+
+editpopup(profileAddButton, "Название", "Ссылка на картинку", popupImage, nameImage, linkImage);
 
 function handleImageSubmit(event) {
   event.preventDefault();
 
   const elementCard = elementTemplate.cloneNode(true);
 
-  elementCard.querySelector('.elements__image').src = infoInput.value;
-  elementCard.querySelector('.elements__image').alt = nameInput.value;
-  elementCard.querySelector('.elements__text').textContent = nameInput.value;
+  elementCard.querySelector('.elements__image').src = linkImage.value;
+  elementCard.querySelector('.elements__image').alt = nameImage.value;
+  elementCard.querySelector('.elements__text').textContent = nameImage.value;
 
-  elementsСards.appendChild(elementCard)
-  popup.classList.remove('popup_opened');
-}
-popupForm.addEventListener('submit', handleImageSubmit);
+  elementsСards.prepend(elementCard)
+  popupImage.classList.remove('popup_opened');
+};
+
+popupImage.addEventListener('submit', handleImageSubmit);
+
+popupContainerImage.querySelector('.popup__close').addEventListener('click', closePopup);
+
+const likeButtons = Array.from(document.querySelectorAll(".elements__like"));
+const likeCounts = Array.from(document.querySelectorAll(".elements__image"));
+
+likeButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    button.classList.toggle("elements__like_active");
+    const current = Number(likeCounts[index].innerHTML);
+    const inc = button.classList.contains("elements__like_active") ? 1 : -1;
+    likeCounts[index].innerHTML = current + inc;
+  });
+});
