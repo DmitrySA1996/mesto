900ced38342e04bc828dc9517049cf0888bcf673
Dmitry Senchenko <114693652+DmitrySA1996@users.noreply.github.com>
2023-01-17 00:26:47 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -88,7 +88,6 @@ function createCard(name, link) {
   elementsImage.addEventListener('click', () => {
 
     openPopup(popupCard);
-
     cardImg.src = link;
     cardImg.alt = elementsImage.alt;
     cardSign.textContent = elementsText.textContent;
@@ -116,6 +115,7 @@ function handleFormSubmit(event) {
   title.textContent = nameInput.value;
   subtitle.textContent = infoInput.value;
   closePopup(popupProfile);
+  hideError(popup);
 };
 
 formProfile.addEventListener('submit', handleFormSubmit);
@@ -128,23 +128,23 @@ function handleImageSubmit(event) {
   event.preventDefault();
   elementCard = createCard(nameImage.value, linkImage.value);
   elementsContainer.prepend(elementCard);
-  closePopup(popupImage);
+  closePopup(popupCard);
   formImage.reset();
 };
 
 formImage.addEventListener('submit', handleImageSubmit);
 
-function closePopup(popup) {
+function hideError(popup) {
   const container = popup.querySelector('.popup__container');
   const formElement = container.querySelector('.popup__form');
-  const inputList = Array.from(formElement.querySelectorAll('.popup__text'));;
-
+  const inputList = Array.from(formElement.querySelectorAll('.popup__text'));
   inputList.forEach((inputElement) => {
     console.log(inputElement);
     hideInputError(formElement, inputElement);
   });
+};
 
-
+function closePopup(popup) {  
 
   enableValidation();
 
@@ -157,78 +157,13 @@ buttonCloseList.forEach(btn => {
   document.addEventListener('keydown', function (evt) {
     if (evt.key === 'Escape') {
       closePopup(popup);
+      hideError(popup);
     }
   });
   popup.addEventListener('click', function (evt) {
     if (evt.target === evt.currentTarget) {
       closePopup(popup);
+      hideError(popup);
     }
   });
 });
-
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  errorElement.classList.add('popup__text-error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__text-error_active');
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup__text-error');
-  errorElement.classList.remove('popup__text-error_active');
-  errorElement.textContent = '';
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__text'));
-  const buttonElement = formElement.querySelector('.popup__submit');
-
-  toggleButtonState(inputList, buttonElement);
-
-  inputList.forEach((inputElement) => {
-
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-    });
-    const fieldsetList = Array.from(formElement.querySelectorAll('.popup__set'));
-
-    fieldsetList.forEach((fieldSet) => {
-      setEventListeners(fieldSet);
-    })
-
-  });
-
-};
-
-function hasInvalidInput(inputList, inputElement) {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-};
-
-function toggleButtonState(inputList, buttonElement) {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add('popup__submit_inactive');
-  } else {
-    buttonElement.classList.remove('popup__submit_inactive');
-  }
-};
